d3589071f36e0966827846abc68bb164e21a4219
[Arin Ramer] <[arinramer@gmail.com]>
2019-12-17 15:15:10 -0800

    not finished

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,17 @@
-// Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+import Dashboard from './Dashboard';
+import Display from '../display/Display';
+import Controls from '../controls/Controls';
+
+test('Dashboard renders correctly', () => {
+    render(<Dashboard/>);
+})
+
+test('Display renders correctly', () => {
+    render(<Display/>);
+})
+
+test('Controls render correctly', () => {
+    render(<Controls/>);
+})
